80b3dfe75be34bf7ba1bccf12652053ff6d63112
Jossany Moura <mjossany@gmail.com>
2022-06-02 13:42:42 -0300

    Create Authentication

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { SignInButton } from './SignInButton';
 import styles from './styles.module.scss';
 
+
 export const Header = () => {  
   return (
     <header className={styles.headerContainer}>
