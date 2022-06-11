0edf30b4c878289071edeb13ee80c043da5afbdc
Jossany Moura <mjossany@gmail.com>
2022-06-11 18:52:47 -0300

    Implement menu navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
-import { SignInButton } from './SignInButton';
-import styles from './styles.module.scss';
+import Link from 'next/link';
 
+import { SignInButton } from './SignInButton';
+
+import styles from './styles.module.scss';
 
 export const Header = () => {  
   return (
@@ -8,8 +10,12 @@ export const Header = () => {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
