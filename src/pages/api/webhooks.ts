0f87bf1f173a36ef7628189d1889f8133c9a7e2c
Jossany Moura <mjossany@gmail.com>
2022-06-29 15:46:15 -0300

    Minor modifications

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -41,7 +41,7 @@ const webhooks = async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(400).send(`Webhook error: ${err.message}`)
     }
 
-    const { type }= event;
+    const { type } = event;
 
     if (relevantEvents.has(type)) {
       try {
